82f528745dfbe391f6663e8764f44f8de40057f0
wnsdn00 <junwoo000706@gmail.com>
2025-03-22 16:57:02 +0900

    비밀번호 유효성 검증 추가

--- javaScript/login.js
+++ javaScript/login.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   const emailInput = document.getElementById("email");
   const emailError = document.getElementById("email-error");
 
+  const passwordInput = document.getElementById("password");
+  const passwordError = document.getElementById("password-error");
+
+  /* 이메일 유효성 검증 */
   emailInput.addEventListener("focusout", function() {
     const emailValue = emailInput.value.trim();
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -12,6 +16,19 @@ document.addEventListener("DOMContentLoaded", function() {
       showError("잘못된 이메일 형식입니다.");
     } else {
       hideError();
+    }
+  });
+
+  /* 비밀번호 유효성 검증 */
+  passwordInput.addEventListener("focusout", function() {
+    const passwordValue = passwordInput.value.trim();
+
+    if (passwordValue === "") {
+      showError("비밀번호를 입력해주세요.");
+    } else if (passwordValue.length < 8) {
+      showError("비밀번호를 8자 이상 입력해주세요.");
+    } else {
+      hideError(passwordInput, passwordError);
     }
   });
 
